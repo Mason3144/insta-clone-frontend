b27707abe3aef5c68d88a46fe5fb6281310b5009
Mason3144 <lts890303@gmail.com>
2022-09-24 13:55:00 +0900

    Login UI

---
+++ src/components/auth/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const SButton = styled.input`
+  border: none;
+  border-radius: 3px;
+  margin-top: 12px;
+  background-color: ${(props) => props.theme.accent};
+  color: white;
+  text-align: center;
+  padding: 8px 0px;
+  font-weight: 600;
+  width: 100%;
+`;
+
+type IProps = {
+  type: string;
+  placeholder: string;
+};
+
+const Button = (props: IProps) => {
+  return <SButton {...props} />;
+};
+
+export default Button;
