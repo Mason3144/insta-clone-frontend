9fb4cede3e836048528a3b029fd42747c3b433fb
Mason3144 <lts890303@gmail.com>
2022-09-27 03:57:15 +0900

    Activating toggleLike button

---
+++ src/components/feed/Photo.tsx
@@ -0,0 +1,122 @@
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import { FatText } from "../shared";
+import Avatar from "../Avatar";
+import { gql, useMutation } from "@apollo/client";
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border-radius: 4px;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 60px;
+  max-width: 615px;
+`;
+const PhotoHeader = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(239, 239, 239);
+`;
+
+const Username = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  width: 100%;
+  max-width: 100%;
+`;
+const PhotoData = styled.div`
+  padding: 12px 15px;
+`;
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+const PhotoAction = styled.div`
+  margin-right: 10px;
+  cursor: pointer;
+`;
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
+`;
+
+interface User {
+  avatar: string;
+  username: string;
+}
+interface IProps {
+  id: number;
+  user: User;
+  file: string;
+  isLiked: boolean;
+  likes: number;
+}
+
+const TOGGLE_LIKE_MUTATION = gql`
+  mutation toggleLike($id: Int!) {
+    toggleLike(id: $id) {
+      ok
+      error
+      token
+    }
+  }
+`;
+
+const Photo = ({ id, user, file, isLiked, likes }: IProps) => {
+  const [toggleLikeMutation, { loading }] = useMutation(TOGGLE_LIKE_MUTATION, {
+    variables: {
+      id,
+    },
+  });
+  return (
+    <PhotoContainer key={id}>
+      <PhotoHeader>
+        <Avatar lg url={user.avatar} />
+        <Username>{user.username}</Username>
+      </PhotoHeader>
+      <PhotoFile src={file} />
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction onClick={() => toggleLikeMutation()}>
+              <FontAwesomeIcon
+                style={{ color: isLiked ? "tomato" : "inherit" }}
+                icon={isLiked ? SolidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </PhotoActions>
+        <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+      </PhotoData>
+    </PhotoContainer>
+  );
+};
+
+export default Photo;
