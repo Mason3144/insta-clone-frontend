309a92533eee1c879886c513a73dc4ce2eb13bc0
Mason3144 <lts890303@gmail.com>
2022-09-27 08:08:18 +0900

    secure Hashtags in posts with sanitize-html

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,9 +1,18 @@
+import sanitize from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
 const CommentContiner = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 interface IProps {
@@ -12,10 +21,19 @@ interface IProps {
 }
 
 const Comment = ({ author, caption }: IProps) => {
+  const cleanedPayload = sanitize(
+    caption?.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>") || "",
+    { allowedTags: ["mark"] }
+  );
+
   return (
     <CommentContiner>
       <FatText>{author}</FatText>
-      <CommentCaption>{caption}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      ></CommentCaption>
     </CommentContiner>
   );
 };
